f871cf39f024a3f54bed294483de8398a7e512e5
Vinicius Costa <42116742+viniciuscomics@users.noreply.github.com>
2020-03-28 11:22:43 -0300

    First flight control project upload.

---
+++ src/main/java/com/atech/FlightControlApplication.java
@@ -0,0 +1,13 @@
+package com.atech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightControlApplication.class, args);
+	}
+
+}
